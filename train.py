c4c89c4288e5895c97f55c0f4ced493bca8f2242
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2022-09-18 11:11:09 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -9,17 +9,7 @@ os.environ["KMP_DUPLICATE_LIB_OK"]  =  "TRUE"
 import torch
 import torch.utils.data
 from torch import nn, optim
-from models.resnet import ResNet,ResBlock
-from models.repVGG import RepVGG,RepVGGBlock
-from models.res2net import Res2Net,Bottle2neck
-from models.VGG16 import VGG16
-from models.shufflenet import *
-from models.efficientnet import *
-from models.mobilenet import *
-from models.mobilenetv2 import *
-from models.lenet import *
-from models.densenet import *
-from models.shufflenetv2 import *
+from util.load_model import load_model
 from tqdm import tqdm
 from util.colorstr import colorstr
 from util.data_loader import load_data
@@ -47,32 +37,6 @@ def main(opts):
     '''Start training model (input train/test datasets(loader)and parameters(opts))'''
     train(model,train_loader,test_loader,opts)
 #--------------------------------------------------------------------------------------------
-def load_model(opts,nc):
-    if opts.model=='resnet' or opts.model=='Resnet' or opts.model=='ResNet':
-        model = ResNet(ResBlock,nc=nc)
-    elif opts.model=='repvgg' or opts.model=='RepVGG' or opts.model=='Repvgg' or opts.model=='RepVgg' or opts.model=='repVgg' or opts.model=='repVGG':
-        model = RepVGG(num_classes=10)
-    elif opts.model=='vgg16' or opts.model=='VGG16' or opts.model=='Vgg16':
-        model = VGG16()
-    elif opts.model=='res2net' or opts.model=='Res2net' or opts.model=='Res2Net':
-        model = Res2Net() 
-    elif opts.model=='shufflenet' or opts.model=='ShuffleNet' or opts.model=='shuffleNet':
-        model = ShuffleNetG2()
-    elif opts.model=='EfficientNet' or opts.model=='efficientNet' or opts.model=='efficientnet':
-        model = EfficientNetB0()
-    elif opts.model=='MobileNet' or opts.model=='Mobilenet' or opts.model=='mobilenet':
-        model = MobileNet()
-    elif opts.model=='MobileNetV2' or opts.model=='MobileNetv2' or opts.model=='mobileNetv2' or opts.model=='mobilenetv2':
-        model = MobileNetV2()
-    elif opts.model=='LeNet' or opts.model=='Lenet' or opts.model=='lenet' or opts.model=='leNet':
-        model = LeNet()
-    elif opts.model=='DenseNet' or opts.model=='Densenet' or opts.model=='denseNet' or opts.model=='densenet':
-        model = densenet_cifar()
-    elif opts.model=='ShuffleNetV2' or opts.model=='shuffleNetV2' or opts.model=='shufflenetV2' or opts.model=='shufflenetv2':
-        model = ShuffleNetV2(net_size=0.5)
-        
-    return model
-#--------------------------------------------------------------------------------------------------------
 def train(model,train_loader,test_loader,opts):
     if torch.cuda.is_available():
         model.cuda() 
